05bb60c46efbebd2107ab44b8deece4c88e209fd
An Cao <kcj.pop.com@gmail.com>
2024-02-02 09:54:24 +0200

    Push artifacts
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- curl.sh
+++ curl.sh
@@ -1,3 +1,3 @@
 while IFS= read -r line; do
-    curl --output-dir 'out' -O -L --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3" "$line"
+    curl --output-dir "out/$FOO" -O -L --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3" "$line"
 done
