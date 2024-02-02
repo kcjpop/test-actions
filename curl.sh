24e740c78c662e68153f868979b54a29b09d158d
An Cao <kcj.pop.com@gmail.com>
2024-02-02 11:24:06 +0200

    Parallel curl
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- curl.sh
+++
@@ -1,3 +0,0 @@
-while IFS= read -r line; do
-    curl --output-dir "out/$FOO" -O -L --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3" "$line"
-done
